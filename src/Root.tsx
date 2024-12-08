de908f98a6ac263fa2d4c1b34143f5023f583c09
Ivanna Zaiets <iva.zaiets@gmail.com>
2024-12-08 09:59:37 +0200

    fix: changed BroswerRouter to HashRouter

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,5 +1,5 @@
 import { StrictMode } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 import HomePage from "./pages/HomePage/HomePage";
 import { Provider } from "react-redux";
