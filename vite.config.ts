0c51392d5b8e8994807f85de35a9254b4feff8a2
Ivanna Zaiets <iva.zaiets@gmail.com>
2024-12-08 10:23:47 +0200

    fix: fixed deploy.yml file / changed vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import dotenv from "dotenv";
 
-// https://vite.dev/config/
+// Завантажує .env файл
+dotenv.config();
+
+console.log("VITE_APP_ID:", process.env.VITE_APP_ID); // Перевірка значення змінної
+
 export default defineConfig({
   plugins: [react()],
   base: "/weather-app/",
+  define: {
+    "process.env": process.env, // Додає змінні середовища у проєкт
+  },
 });
